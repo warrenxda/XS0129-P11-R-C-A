32bfda7fe4a14b40667ff09b359812f1339b077c
Camille <camiroqui16193@gmail.com>
2026-05-26 21:57:37 -0600

    README realizado

--- Práctica_Semana_11_-_Graficos_en_R.R
+++ Práctica_Semana_11_-_Graficos_en_R.R
@@ -266,8 +266,6 @@ ggplot(airquality, aes(x = Temp, y = Ozone, colour = factor(Month))) +
     x = "Temperatura",
     y = "Nivel de ozono")
 
-#Consultar a Cami
-
 # Comentario 2.2: Agregar un color a cada mes en el grafico ayuda a identificar a 
 #                 caracteristicas de los valores en el grafico, como lo puede ser una tendencia.
 
